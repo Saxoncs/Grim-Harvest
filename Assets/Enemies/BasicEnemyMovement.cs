4052ca93bba9a09e4d4f515124eafd8ec7343f59
Saxoncs <saxon.cs@outlook.com>
2020-07-10 19:19:03 +1000

    Rewrite BasicEnemyMovement as subclass of Enemy
    This might need to be rewinded if it doesn't work out, but initial look is promising!

--- Assets/Enemies/BasicEnemyMovement.cs
+++ Assets/Enemies/BasicEnemyMovement.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicEnemyMovement : MonoBehaviour
+public class BasicEnemyMovement : Enemy
 {
 
-    private Vector3 startingLocation;
-    private Vector2 startingCoordinates;
-    private Vector3 currentLocation;
 
     // The range at which the bad boy will aggro to the good boy
     [SerializeField] private float aggroRange;
@@ -21,79 +18,26 @@ public class BasicEnemyMovement : MonoBehaviour
     Vector2 randomDirection;
     //the destination as a 3d vector
     [SerializeField] Vector3 randomDestination;
-    //The amount of time the enemy will stop for before changing directions
-    [SerializeField] private float waitTime;
-    //checks whether the enemy is waiting or not, should be replaced with a state system
-    private bool waiting;
-    // Hitbox data for the player
-    private Hitbox hitbox;
-
-    //track the player
-    [SerializeField] GameObject player;
 
     private Vector3 playerLocation;
 
-    //determines if the enemy is vulnerable or not
-    private bool vulnerable = true;
-
-    //how long an enemy remains invulnerable after being hit
-    [SerializeField] private float hitInvulnerablility;
-
-    //Enemy health
-    [SerializeField] private float health;
-
-    private void Awake()
-    {
-        startingLocation = transform.position;
-        startingCoordinates.x = transform.position.x;
-        startingCoordinates.y = transform.position.z;
-
-        
-
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        waiting = false;
         PickDestination();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        currentLocation = transform.position;
-        playerLocation = player.transform.position;
 
-        if (health <= 0)
-        {
-            Death();
-        }
-
-        //Check for player in range
-        if (CheckForPlayer())
-        {
-            //Move towards player if in range
-            MoveToPlayer();
-        }
-        else
-        {
-            //Patrol randomly if player not in range
-            Patrol();
-        }
-
-
-
-
-    }
-
-    bool CheckForPlayer()
+    //overrides from enemy class
+    override bool CheckForPlayer()
     {
         if (Vector3.Distance(playerLocation, currentLocation) < aggroRange) return true;
         else return false;
     }
 
-    void MoveToPlayer()
+    //overrides from enemy class
+    override void MoveToPlayer()
     {
         if (!waiting)
         {
@@ -110,7 +54,8 @@ public class BasicEnemyMovement : MonoBehaviour
         }
     }
 
-    void Patrol()
+    // overrides from enemy class
+    override void Patrol()
     {
         if (!waiting)
         {
